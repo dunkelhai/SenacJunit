b25facc1aef33b7539eb086351d1016591733105
Alysson <3oalysson@gmail.com>
2023-06-26 21:13:34 -0300

    Ajustes novos

--- src/main/java/com/senac/Main.java
+++ src/main/java/com/senac/Main.java
@@ -2,7 +2,9 @@ package com.senac;
 
 public class Main {
     public static void main(String[] args) {
-        int resultado = Calculadora.soma(2,2);
-        System.out.println(resultado);
+//        int resultado = Calculadora.soma(2,2);
+//        System.out.println(resultado);
+        boolean retorno = ParImpar.validaPar(5);
+        System.out.println(retorno);
     }
 }
