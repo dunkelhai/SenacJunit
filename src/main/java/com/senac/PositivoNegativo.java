80c36415ce0dbc3264029770e3ec34abcf0304da
Alysson Borges <82451374+dunkelhai@users.noreply.github.com>
2026-06-01 21:47:40 -0300

    teste

--- src/main/java/com/senac/PositivoNegativo.java
+++ src/main/java/com/senac/PositivoNegativo.java
@@ -1,5 +1,7 @@
 package com.senac;
 
+import java.util.Locale;
+
 public class PositivoNegativo {
     public static String avalia(double n1){
         if (n1 > 0){
@@ -9,5 +11,19 @@ public class PositivoNegativo {
         } else {
             return "Zero";
         }
+    }
+    public static String paraDarErro(String teste) throws InterruptedException{
+        Thread.sleep(10000L);
+        return teste.toLowerCase(Locale.ROOT).toUpperCase();
+    }
+
+    public static String tete(String teste) throws InterruptedException{
+        Thread.sleep(10000L);
+        return teste.toLowerCase(Locale.ROOT).toUpperCase();
+    }
+
+    public static String testete(String teste) throws InterruptedException{
+        Thread.sleep(10000L);
+        return teste.toLowerCase(Locale.ROOT).toUpperCase();
     }
 }
--- src/test/java/com/senac/PositivoNegativoTest.java
+++ src/test/java/com/senac/PositivoNegativoTest.java
@@ -1,15 +1,39 @@
 package com.senac;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 class PositivoNegativoTest {
 
     @Test
+    //falha, assert -> diferente do esperado
     void deveResultarPositivoQuandoNumeroMaiorQueZero() {
         String resultado = PositivoNegativo.avalia(1.0);
         Assertions.assertEquals("Positivo", resultado);
     }
+
+    @Disabled
+    @Test
+    void ignorar() {
+       assertTrue(true);
+    }
+
+    @Test
+    void erro() throws InterruptedException{
+        String resultado = PositivoNegativo.paraDarErro(null);
+        Assertions.assertEquals("Negativo", resultado);
+    }
+
+    @Test
+    void sucesso() {
+        assertFalse(false);
+    }
+
     @Test
     void avaliaNegativo() {
         String resultado = PositivoNegativo.avalia(-1.0);
